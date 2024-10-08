6c55751dbd93c840d464284c299a5282b1520721
tacserus <115652334+tacserus@users.noreply.github.com>
2024-10-09 02:48:52 +0500

    Router and State

--- maven.eclipse/src/main/java/dnd/bot/maven/eclipse/Bot/Bot.java
+++ maven.eclipse/src/main/java/dnd/bot/maven/eclipse/Bot/Bot.java
@@ -1,11 +1,17 @@
-package dnd.bot.maven.eclipse;
+package dnd.bot.maven.eclipse.Bot;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageReplyMarkup;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import dnd.bot.maven.eclipse.Interfaces.ICommand;
 import dnd.bot.maven.eclipse.Commands.*;
@@ -26,6 +32,7 @@ public class Bot extends TelegramLongPollingBot{
 	    var msg = update.getMessage();
 	    var user = msg.getFrom();
 	    var id = user.getId();
+			
 	    if(msg.isCommand()) 
 	    {
 	    	var command = commands.get(msg.getText());
@@ -49,13 +56,13 @@ public class Bot extends TelegramLongPollingBot{
 	}
 	
 	public void sendText(Long who, String what){
-		   SendMessage sm = SendMessage.builder()
-		                    .chatId(who.toString()) //Who are we sending a message to
-		                    .text(what).build();    //Message content
-		   try {
-		        execute(sm);                        //Actually sending the message
-		   } catch (TelegramApiException e) {
-		        throw new RuntimeException(e);      //Any error will be printed here
-		   }
+		SendMessage sm = SendMessage.builder()
+						.chatId(who.toString()) //Who are we sending a message to
+						.text(what).build();    //Message content
+		try {
+			execute(sm);                        //Actually sending the message
+		} catch (TelegramApiException e) {
+			throw new RuntimeException(e);      //Any error will be printed here
 		}
+	}
 }
---
+++ maven.eclipse/src/main/java/dnd/bot/maven/eclipse/Routing/Router.java
@@ -0,0 +1,39 @@
+package dnd.bot.maven.eclipse.Routing;
+import java.util.Stack;
+
+
+public class Router {
+    private static final Router router = new Router();
+    private static final Stack<State> transitionsStack = new Stack<State>();
+
+    private Router() {
+    }
+
+    public static Router getInstance() {
+        return router;
+    }
+
+    public State getCurrentState() {
+        return transitionsStack.peek();
+    }
+
+    public void MakeTransition(String callBackCommand) {
+        if (callBackCommand == "back")
+        {
+            transitionsStack.pop();
+            transitionsStack.peek().render();
+            return;
+        }
+
+        var currentState = transitionsStack.peek();
+
+        if (!currentState.possibleTransitions.containsKey(callBackCommand)) {
+            throw new IllegalArgumentException("wrong callBack");
+        }
+
+        var nextState = currentState.possibleTransitions.get(callBackCommand);
+
+        transitionsStack.add(nextState);
+        nextState.render();
+    }
+}   
