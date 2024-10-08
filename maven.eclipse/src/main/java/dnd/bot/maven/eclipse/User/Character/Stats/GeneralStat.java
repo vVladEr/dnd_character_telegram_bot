63921813c9ccfa89f520355242698dd9eea7f930
egorpopov676767 <e.e.popov@urfu.me>
2024-10-08 20:36:44 +0500

    classes

---
+++ maven.eclipse/src/main/java/dnd/bot/maven/eclipse/User/Character/Stats/GeneralStat.java
@@ -0,0 +1,15 @@
+package dnd.bot.maven.eclipse.User.Character.Stats;
+
+import dnd.bot.maven.eclipse.User.Character.Stats.Skills.Skill;
+
+import java.util.Dictionary;
+
+public abstract class GeneralStat {
+	public String name;
+	public int value;
+	public Dictionary<String, Skill> skills;
+
+	public abstract int checkValue();
+
+	public abstract void saveThrowValue();
+}
